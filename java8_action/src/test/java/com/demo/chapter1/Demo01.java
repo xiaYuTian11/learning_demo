e255001d3427c74a568f236fbbec05860efc61b8
xiaYuTian11 <875618779@qq.com>
2019-12-29 19:34:02 +0800

    1.Java8实战 --> 第一章；

--- java8_action/src/test/java/com/demo/chapter1/Demo01.java
+++ java8_action/src/test/java/com/demo/chapter1/Demo01.java
@@ -3,6 +3,10 @@ package com.demo.chapter1;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 /**
  * @author TMW
@@ -13,11 +17,31 @@ public class Demo01 {
     public void test01() {
         File[] files = new File(".").listFiles(File::isHidden);
     }
+
     @Test
-    public void test02(){
+    public void test02() {
         // List<Apple> inventory = Arrays.asList(new Apple(80,"green"),
         //         new Apple(155, "green"),
         //         new Apple(120, "red"));
 
+    }
+
+    @Test
+    public void test03() {
+        System.out.println(filter("String", str -> str.equals("String1")));
+    }
+
+    public boolean filter(String str, Predicate<String> predicate) {
+        return predicate.test(str);
+    }
+
+    @Test
+    public void test04() {
+        List<String> list = new ArrayList<>();
+        Predicate<String> p = s -> list.add(s);
+        Consumer<String> c = s -> list.add(s);
+        System.out.println(p);
+        System.out.println(c);
+        System.out.println(list);
     }
 }
