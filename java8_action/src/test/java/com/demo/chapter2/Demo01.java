24fc0c8ce2d436953754c50a97955a2968ce8aac
tmw <2998662089@qq.com>
2020-01-08 16:23:06 +0800

    1.创建第二章节的实体类；

--- java8_action/src/test/java/com/demo/chapter2/Demo01.java
+++ java8_action/src/test/java/com/demo/chapter2/Demo01.java
@@ -3,7 +3,11 @@ package com.demo.chapter2;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author TMW
@@ -28,7 +32,27 @@ public class Demo01 {
 
     @Test
     public void test01() {
+        Map<String, String> map = new HashMap<>();
+        String s = map.get(null);
+        System.out.println(s);
+    }
 
+    @Test
+    public void test02() {
+        String[] words = new String[]{"Hello", "World"};
+        List<String[]> collect = Arrays.stream(words).map(word -> word.split("")).distinct().collect(Collectors.toList());
+        System.out.println(Arrays.asList(collect));
+        List<Stream<String>> streamList = Arrays.stream(words).map(word -> word.split("")).map(Arrays::stream).collect(Collectors.toList());
+        List<String> stringList = Arrays.stream(words).map(word -> word.split("")).flatMap(Arrays::stream).distinct().collect(Collectors.toList());
+        System.out.println(stringList);
+    }
+
+    @Test
+    public void test03() {
+        List<Integer> numbers1 = Arrays.asList(1, 2, 3);
+        List<Integer> numbers2 = Arrays.asList(3, 4);
+        List<Stream<Integer[]>> streamList = numbers1.stream().map(num1 -> numbers2.stream().map(num2 -> new Integer[]{num1, num2})).collect(Collectors.toList());
+        List<Integer[]> collect = numbers2.stream().flatMap(num1 -> numbers2.stream().map(num2 -> new Integer[]{num1, num2})).collect(Collectors.toList());
 
     }
 
