d824ec40df613881dd0cae52a5ef01248dd09c34
xiaYuTian11 <875618779@qq.com>
2020-03-16 11:37:37 +0800

    反射创建对象的三种方式

---
+++ java_demo/src/test/java/com/demo/base/BinarySearch.java
@@ -0,0 +1,8 @@
+package com.demo.base;
+
+/**
+ * @author TMW
+ * @since 2020/3/14 14:15
+ */
+public class BinarySearch {
+}
---
+++ java_demo/src/test/java/com/demo/base/Hello.java
@@ -0,0 +1,8 @@
+package com.demo.base;
+
+/**
+ * @author TMW
+ * @since 2020/3/16 9:50
+ */
+public class Hello {
+}
