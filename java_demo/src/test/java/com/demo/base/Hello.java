8ebdbeb2a8c77b54e00ed87418e7b0488ebb0c30
tmw <2998662089@qq.com>
2020-06-05 11:50:04 +0800

    1.添加中文乱码处理；

--- java_demo/src/test/java/com/demo/base/Hello.java
+++ java_demo/src/test/java/com/demo/base/Hello.java
@@ -1,7 +1,12 @@
 package com.demo.base;
 
+import cn.hutool.core.util.StrUtil;
+import org.junit.jupiter.api.Test;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author TMW
@@ -24,5 +29,23 @@ public class Hello {
         while (true){
             list.add(new Hello());
         }
+    }
+
+    @Test
+    public void test01(){
+        String one = "02";
+        String two = null;
+        String three = "13";
+
+        String join = String.join(",", one, two, three);
+        System.out.println(join);
+        List<String> stringList = Stream.of(one, two, three).filter(StrUtil::isNotBlank).collect(Collectors.toList());
+        join = String.join(",",stringList);
+        System.out.println(join);
+        if (StrUtil.isBlank(one) && StrUtil.isBlank(two) && StrUtil.isBlank(three)) {
+            // 群众
+            System.out.println(join);
+        }
+
     }
 }
