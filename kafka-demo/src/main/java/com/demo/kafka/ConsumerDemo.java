60328779bb95bc1d0d9a50d74b986e293ceb302f
xiaYuTian11 <875618779@qq.com>
2020-10-29 10:04:11 +0800

    1.kafka demo

--- kafka-demo/src/main/java/com/demo/kafka/ConsumerDemo.java
+++ kafka-demo/src/main/java/com/demo/kafka/ConsumerDemo.java
@@ -4,12 +4,9 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.time.Duration;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Properties;
 
@@ -24,11 +21,11 @@ public class ConsumerDemo {
         Properties prop = new Properties();
         prop.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.119.111:9092");
         prop.put(ConsumerConfig.GROUP_ID_CONFIG, "test");
-        prop.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+        // prop.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
         prop.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
         prop.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         prop.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-
+        prop.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
         consumer = new KafkaConsumer<>(prop);
 
     }
@@ -39,6 +36,12 @@ public class ConsumerDemo {
             ConsumerRecords<String, String> poll = consumer.poll(Duration.ofMillis(1000));
             for (ConsumerRecord<String, String> record : poll) {
                 System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+                System.out.println("当前消费偏移量：" + record.offset());
+                consumer.commitSync(); // 同步提交
+                // consumer.committed()
+                // consumer.commitAsync((offsets, exception) -> {
+                //     // 异步提交
+                // });
             }
         }
     }
